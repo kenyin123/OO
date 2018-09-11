2ac5ee8a7f8d02961c4557fcc1d2ea532ab08a10
kenneth <1664334639@qq.com>
2018-09-12 00:29:46 +0800

    make some correction

--- Poly/src/FormatProcess.java
+++ Poly/src/FormatProcess.java
@@ -1,5 +1,6 @@
 import java.util.Vector;
 public class FormatProcess {
+    /*
     public int stringToInt(String numstr) {
         int num;
         int len = numstr.length();
@@ -12,30 +13,31 @@ public class FormatProcess {
         else num = Integer.parseInt(numstr.substring(0, len-1));
         return num;
     }
+    */
 	public void removeBracket(String polynomial,Vector v1) {
 		int len = polynomial.length();
-		polynomial = polynomial.substring(1,len); //maybe wrong
-		String strArray[] = polynomial.split(",");
-		String numArray[];
+		polynomial = polynomial.substring(1,len-1); //maybe wrong
+		//not be familiar with String.length() and String.substring()
+		//this is to remove"{" and "}"
+		polynomial = polynomial.replaceAll("(","");
+		polynomial = polynomial.replaceAll(")","");
+		String[] strArray = polynomial.split(",");
+		//String[] numArray;
 		for (String tmp:strArray) {
-			len = tmp.length();
-			tmp = tmp.substring(1,len);
-			numArray = tmp.split(",");
-			v1.addElement(stringToInt(numArray[0]));
-			v1.addElement(stringToInt(numArray[1]));
+			v1.addElement(Integer.parseInt(tmp));
 		}
 	}
 	public void symbolHandle(String stringInput,Vector v1) {
 		int i=0,indexBefore=0;
 		String tmpString;
-		for (i = 0; i < stringInput.length(); i++) {    
+		for (i = 0; i < stringInput.length()-1; i++) {    
 			if((stringInput.charAt(i) == '+'| stringInput.charAt(i) == '-')&&(i!=stringInput.length())) {
 			    tmpString = stringInput.substring(indexBefore, i-1);
 			    removeBracket(tmpString,v1);
 			    v1.addElement(stringInput.charAt(i));
 			    indexBefore = i+1;
 			}
-			if(i == stringInput.length()) {
+			if(i == stringInput.length()-1) {
 			    tmpString = stringInput.substring(indexBefore, i);
                 removeBracket(tmpString,v1);
                 v1.addElement(stringInput.charAt(i));
--- Poly/src/Project1.java
+++ Poly/src/Project1.java
@@ -1,31 +1,17 @@
 import java.util.Scanner;
 import java.util.Vector;
 public class Project1 {
-	/*
-	public int[] getnum1(String str) //throws IOException
-	{
-		String replaceStr=str.substring(1,str.length()-1);
-		String[] array = replaceStr.split(",");
-		if(array.length!=2) {
-			//throw new IOException();
-		}
-		int a = Integer.parseInt(array[0]);
-		int b = Integer.parseInt(array[1]);
-		int arrayint[] = {a,b};
-		return arrayint;
-	}
-	*/
     public static void main(String[] args) {
-		Scanner Stringinput=new Scanner(System.in);
-		String stringInput=Stringinput.nextLine();
-		stringInput=stringInput.replaceAll(" ","");
-		
-		Vector v1 = new Vector();
-		FormatProcess e1 = new FormatProcess();
-		e1.symbolHandle(stringInput,v1);
-		Calculator e2 = new Calculator();
-		e2.calculate(v1);
-		System.out.println("The Vector v1 is:\n\t"+v1); 
+        Scanner Stringinput=new Scanner(System.in);
+        String stringInput=Stringinput.nextLine();
+        stringInput=stringInput.replaceAll(" ","");
+		 
+        Vector v1 = new Vector();
+        FormatProcess e1 = new FormatProcess();
+        e1.symbolHandle(stringInput,v1);
+        Calculator e2 = new Calculator();
+        e2.calculate(v1);
+        System.out.println("The Vector v1 is:\n\t"+v1); 
 	}
     
 }
